a6f7c3196950042d7529f219b422287f53ab32a8
ubaidalishaikh <161380272+ubaidalishaikh@users.noreply.github.com>
2024-09-20 01:04:04 +0500

    Add files via upload

---
+++ milestone Adding Image/main.ts
@@ -0,0 +1,91 @@
+// Listing Element
+
+document.getElementById('resumeForm')?.addEventListener('submit' , function(event){
+    event.preventDefault();
+
+
+// Type assertion
+
+
+const profilePictureInput = document.getElementById("profilePicture") as HTMLInputElement
+const nameElement = document.getElementById('name') as HTMLInputElement;
+const emailElement = document.getElementById('email') as HTMLInputElement;
+const phoneElement = document.getElementById('phone') as HTMLInputElement;
+const educationElement = document.getElementById('education') as HTMLInputElement;
+const experienceElement = document.getElementById('experience') as HTMLInputElement;
+const skillsElement = document.getElementById('skills') as HTMLInputElement;
+
+if(profilePictureInput && nameElement && emailElement && phoneElement && educationElement && experienceElement && skillsElement){
+    const name = nameElement.value;
+    const email = emailElement.value;
+    const phone = phoneElement.value;
+    const education = educationElement.value;
+    const experience = experienceElement.value;
+    const skills = skillsElement.value;
+
+// picture Elements
+
+const profilePictureFile = profilePictureInput.files?.[0]
+const profilePictureURL = profilePictureFile? URL.createObjectURL(profilePictureFile) :""; 
+
+
+// Create Resume Output
+
+const resumeOutput = `
+<h2>Resume</h2>
+${profilePictureURL ? `<img src="${profilePictureURL}" alt="profilePicture" class="profilePicture">` : '' }
+<p><strong>Name:</strong><span id="edit-name" class="editable">${name}</span></p>
+<p><strong>Email:</strong><span id="edit-email" class="editable">${email}</span></p>
+<p><strong>Phone:</strong><span id="edit-phone" class="editable">${phone}</span></p>
+
+<h3>Education</h3>
+<p id="edit-education" class="editable">Education:${education}</p>
+
+<h3>Experience</h3>
+<p id="edit-experience" class="editable">Experience:${experience}</p>
+
+<h3>Skills</h3>
+<p id="edit-name" class="skills">Skills:${skills}</p>
+`;
+
+
+const resumeOutputElement = document.getElementById('resumeOutput')
+if(resumeOutputElement){
+    resumeOutputElement.innerHTML = resumeOutput;
+makeEditable();
+}
+}else{
+    console.error('Resume Output element not found')
+}
+});
+
+
+
+function makeEditable(){
+    const editableElements = document.querySelectorAll('.editable')
+    editableElements.forEach(element => {
+        element.addEventListener('click' , function() {
+            const currentElement = element as HTMLElement;
+            const currentValue = currentElement.textContent || "" ;
+
+            // Replace Content
+            if(currentElement.tagName === "p" || currentElement.tagName === "span") {
+                const input = document.createElement('input')
+                input.type = 'text'
+                input.value = currentValue
+                input.classList.add('editing-input')
+
+                input.addEventListener('blur' , function(){
+                    currentElement.textContent = input.value;
+                    currentElement.style.display = 'inline'
+                    input.remove()
+                })
+
+                
+                currentElement.style.display = "none"
+                currentElement.parentNode?.insertBefore(input,currentElement)
+                input.focus()
+            }
+        })
+    })
+}
